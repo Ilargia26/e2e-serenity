0dcd9ed69b4cf3ee341c5e22d00d1cc659dd2447
Ilargia26 <mmonge@oshyn.com>
2023-12-12 20:59:12 -0500

    E2E Tests GoDuck

---
+++ src/test/java/starter/actions/SearchActions.java
@@ -0,0 +1,8 @@
+import net.serenitybdd.core.steps.UIInteractions;
+import org.openqa.selenium.Keys;
+
+public class SearchActions extends UIInteractions {
+    public void byKeyword(String keyword) {
+        $("#searchbox_input").sendKeys(keyword, Keys.ENTER);
+    }
+}
